9e6fc17b8f755f09e5446281777d8e64696a9d3e
zeddrix <37854513+zeddrix@users.noreply.github.com>
2020-11-16 10:25:01 +0800

    Created dummy data.

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-
+const data = [
+  {
+    name: "John Doe",
+    age: 32,
+    gender: "male",
+    lookingFor: "female",
+    location: "Boston MA",
+    image: "https://randomuser.me/api/portraits/men/82.jpg",
+  },
+  {
+    name: "Jen Smith",
+    age: 26,
+    gender: "female",
+    lookingFor: "male",
+    location: "Miami FL",
+    image: "https://randomuser.me/api/portraits/women/82.jpg",
+  },
+  {
+    name: "William Johnson",
+    age: 38,
+    gender: "male",
+    lookingFor: "female",
+    location: "Lynn MA",
+    image: "https://randomuser.me/api/portraits/men/83.jpg",
+  },
+];
